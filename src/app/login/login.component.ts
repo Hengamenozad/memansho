427982e0da75e1bab9384b582367361bdfe179f4
mohammadreza <reyhan.atashi@outloo.com>
2025-12-31 15:37:37 +0330

    login updated.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { InputOtpModule } from 'primeng/inputotp';
+import { ColorPickerModule } from 'primeng/colorpicker';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule, InputOtpModule, ColorPickerModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
+  currentStep: any ;
   phoneNumber: string = '';
+  value: any = '' ;
+  color : any = '';
+  ngOnInit() {
+    this.currentStep = 2;
+    
+  }
   login() {
   }
   orAction() {
@@ -17,4 +29,5 @@ export class LoginComponent {
   }
 
 }
+
 
