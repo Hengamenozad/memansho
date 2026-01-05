1b5fd1e5d5a976d7c03b31dcba3f4c9132e9a829
mohammadreza <reyhan.atashi@outloo.com>
2026-01-05 16:18:28 +0330

    dashboard implemented!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
         path: '',
         loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
     }
+     {
+        path: 'dashboard',
+        loadComponent: () => import('./components/dashboard/dashboard.component').then(m => m.DashboardComponent)
+    }
 ];
